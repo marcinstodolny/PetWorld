de6911bc7e63f60aa50758b7b15fd2deec097359
Marcin Stodolny <marcin.stodolny@o2.pl>
2026-02-09 01:10:56 +0100

    fixed an issue with change of categories names

--- src/PetWorld.Infrastructure/Persistence/ProductSeedData.cs
+++ src/PetWorld.Infrastructure/Persistence/ProductSeedData.cs
@@ -10,7 +10,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("6bf43c6c-2c24-4d82-9c2f-05a1e7c49a06"),
             name: "Royal Canin Adult Dog 15kg",
-            category: ProductCategory.KarmaDlaPsow,
+            category: ProductCategory.DogFood,
             priceAmount: 289m,
             priceCurrency: "PLN",
             description: "Premium karma dla dorosłych psów średnich ras"
@@ -18,7 +18,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("3a7c1ad6-6fb4-4f9a-9a2d-7c11df74e6ff"),
             name: "Whiskas Adult Kurczak 7kg",
-            category: ProductCategory.KarmaDlaKotow,
+            category: ProductCategory.CatFood,
             priceAmount: 129m,
             priceCurrency: "PLN",
             description: "Sucha karma dla dorosłych kotów z kurczakiem"
@@ -26,7 +26,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("e3fa1b9b-3758-46f1-8f3a-5f0c9a2a0e9e"),
             name: "Tetra AquaSafe 500ml",
-            category: ProductCategory.Akwarystyka,
+            category: ProductCategory.Aquatics,
             priceAmount: 45m,
             priceCurrency: "PLN",
             description: "Uzdatniacz wody do akwarium, neutralizuje chlor"
@@ -34,7 +34,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("f2a39b45-1f77-4c0b-a4b1-8a6b5d2c9a18"),
             name: "Trixie Drapak XL 150cm",
-            category: ProductCategory.AkcesoriaDlaKotow,
+            category: ProductCategory.CatAccessories,
             priceAmount: 399m,
             priceCurrency: "PLN",
             description: "Wysoki drapak z platformami i domkiem"
@@ -42,7 +42,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("4e2f0a6b-8c17-4c65-96f1-1d7f0b5a2c3e"),
             name: "Kong Classic Large",
-            category: ProductCategory.ZabawkiDlaPsow,
+            category: ProductCategory.DogToys,
             priceAmount: 69m,
             priceCurrency: "PLN",
             description: "Wytrzymała zabawka do napełniania smakołykami"
@@ -50,7 +50,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("9b1f4d62-9f6e-4b0e-8f42-4b2c7c4e2e6a"),
             name: "Ferplast Klatka dla chomika",
-            category: ProductCategory.Gryzonie,
+            category: ProductCategory.SmallPets,
             priceAmount: 189m,
             priceCurrency: "PLN",
             description: "Klatka 60x40cm z wyposażeniem"
@@ -58,7 +58,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("1ac2d2c4-8e26-4d1f-9a0a-93db3f5e2b79"),
             name: "Flexi Smycz automatyczna 8m",
-            category: ProductCategory.AkcesoriaDlaPsow,
+            category: ProductCategory.DogAccessories,
             priceAmount: 119m,
             priceCurrency: "PLN",
             description: "Smycz zwijana dla psów do 50kg"
@@ -66,7 +66,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("2b5a69fd-4d44-4c32-bf5d-7f6b2a5d7f31"),
             name: "Brit Premium Kitten 8kg",
-            category: ProductCategory.KarmaDlaKotow,
+            category: ProductCategory.CatFood,
             priceAmount: 159m,
             priceCurrency: "PLN",
             description: "Karma dla kociąt do 12 miesiąca życia"
@@ -74,7 +74,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("4c6f2a1b-5e7a-45ad-8b2f-63e2c9b1a7a4"),
             name: "JBL ProFlora CO2 Set",
-            category: ProductCategory.Akwarystyka,
+            category: ProductCategory.Aquatics,
             priceAmount: 549m,
             priceCurrency: "PLN",
             description: "Kompletny zestaw CO2 dla roślin akwariowych"
@@ -82,7 +82,7 @@ internal static class ProductSeedData
         new SeedItem(
             id: Guid.Parse("5ad1d7e0-6d9c-4c0c-bf9a-0d84e2b2f4ef"),
             name: "Vitapol Siano dla królików 1kg",
-            category: ProductCategory.Gryzonie,
+            category: ProductCategory.SmallPets,
             priceAmount: 25m,
             priceCurrency: "PLN",
             description: "Naturalne siano łąkowe, podstawa diety"
